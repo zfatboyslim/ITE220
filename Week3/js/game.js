611b2209c43ef95237c5760e815e5a16f0154a4c
Thomas Charlesworth <irexjrs@gmail.com>
2015-12-02 10:14:44 +0700

    week3 guess

--- Week3/js/game.js
+++ Week3/js/game.js
@@ -1,5 +1,5 @@
 var game = {
-	answer: 50, // Need to random this value from 0-100
+	answer: Math.floor((Math.random() * 100) + 1), // Need to random this value from 0-100
 	init: function() {
 		var self = this;
 		// Number 1
@@ -14,17 +14,74 @@ var game = {
 			self.click(2);
 		}, false);
 
+		// Number 3
+		var ele = document.getElementById("three");
+		ele.addEventListener("click", function(){
+			self.click(3);
+		}, false);
+
+		// Number 4
+		var ele = document.getElementById("four");
+		ele.addEventListener("click", function(){
+			self.click(4);
+		}, false);
+
+		// Number 5
+		var ele = document.getElementById("five");
+		ele.addEventListener("click", function(){
+			self.click(5);
+		}, false);
+
+		// Number 6
+		var ele = document.getElementById("six");
+		ele.addEventListener("click", function(){
+			self.click(6);
+		}, false);
+
+		// Number 7
+		var ele = document.getElementById("seven");
+		ele.addEventListener("click", function(){
+			self.click(7);
+		}, false);
+
+		// Number 8
+		var ele = document.getElementById("eight");
+		ele.addEventListener("click", function(){
+			self.click(8);
+		}, false);
+
+		// Number 9
+		var ele = document.getElementById("nine");
+		ele.addEventListener("click", function(){
+			self.click(9);
+		}, false);
+
+		// Number 0
+		var ele = document.getElementById("zero");
+		ele.addEventListener("click", function(){
+			self.click(0);
+		}, false);
+
 		// Guess!
 		var ele = document.getElementById("guess");
-		ele.addEventListener("click", this.checkAnswer, false);
+		//ele.addEventListener("click", this.checkAnswer, false);
+		ele.addEventListener("click", function(){
+			self.checkAnswer(self);
+		}, false);
 	},
 	click: function(value) {
 		console.log(value + " clicked.");
 		var userValueEle = document.getElementById("userValue");
 		userValueEle.value += value;
 	},
-	checkAnswer: function() {
-		alert("This is not correct! Try again. ");
+	checkAnswer: function(self) {
+		var userValueEle = document.getElementById("userValue");
+		if(userValueEle.value == self.answer){
+		alert("This answer is correct");
+		}
+		else { 	
+			userValueEle.value = "";
+		}	
 	}
 };
 
