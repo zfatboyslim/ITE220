617e5741149d820fddc057bf3340af04020ded12
Thomas Charlessworth <irexjrs@gmail.com>
2016-01-13 22:14:19 +0700

    Weather HW

--- Week7/js/app.js
+++ Week7/js/app.js
@@ -1,19 +1,58 @@
 //Thomas Charlesworth 213210087
 $(function(){
-	$("a").on("click", function(e){
+	$("#cityone").on("click", function(e){
 		e.preventDefault();
 		//alert(this.text);
 		var city = this.text.toLowerCase();
 		$.ajax({
 			type: 'GET',
+			dataType: 'jsonp',
 			url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=2de143494c0b295cca9337e1e96b00e0',
 		success: function(data){
 			console.log(data);
-			$("#container").html(data.name + " " + data.main.temp);
+			var celcius = data.main.temp - 273;
+			var fahrenheit = celcius * 1.8 + 32;
+			fahrenheit = fahrenheit.toFixed(2);
+			celcius = celcius.toFixed(2);
+			$("#containerone").html("<br>Temperature Celcius: " + celcius + "<br>Temperature Fahrenheit: " + fahrenheit +"<br> Humidity: " + data.main.humidity);
 		}
 		});
-
-
-
 	});
+	$("#citytwo").on("click", function(e){
+		e.preventDefault();
+		//alert(this.text);
+		var city = this.text.toLowerCase();
+		$.ajax({
+			type: 'GET',
+			dataType: 'jsonp',
+			url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=2de143494c0b295cca9337e1e96b00e0',
+		success: function(data){
+			console.log(data);
+			var celcius = data.main.temp - 273;
+			var fahrenheit = celcius * 1.8 + 32;
+			fahrenheit = fahrenheit.toFixed(2);
+			celcius = celcius.toFixed(2);
+			$("#containertwo").html("<br>Temperature Celcius: " + celcius + "<br>Temperature Fahrenheit: " + fahrenheit +"<br> Humidity: " + data.main.humidity);
+		}
+		});
+	});
+	$("#citythree").on("click", function(e){
+		e.preventDefault();
+		//alert(this.text);
+		var city = this.text.toLowerCase();
+		$.ajax({
+			type: 'GET',
+			dataType: 'jsonp',
+			url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=2de143494c0b295cca9337e1e96b00e0',
+		success: function(data){
+			console.log(data);
+			var celcius = data.main.temp - 273;
+			var fahrenheit = celcius * 1.8 + 32;
+			fahrenheit = fahrenheit.toFixed(2);
+			celcius = celcius.toFixed(2);
+			$("#containerthree").html("<br>Temperature Celcius: " + celcius + "<br>Temperature Fahrenheit: " + fahrenheit +"<br> Humidity: " + data.main.humidity);
+		}
+		});
+	});
+
 });
